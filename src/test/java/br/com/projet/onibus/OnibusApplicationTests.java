1cded5b39f475fc71dda4014623ff451ed7318c1
KlaucioDamasceno <klaucio.etb@gmail.com>
2023-06-21 16:41:56 -0300

    Create Project

---
+++ src/test/java/br/com/projet/onibus/OnibusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projet.onibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnibusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
